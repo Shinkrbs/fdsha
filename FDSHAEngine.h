7d0ffd3bf1857346558e2ae64fa9a9818547d7e9
Shinkrbs <kerbydolamoscs@gmail.com>
2025-10-09 18:20:32 +0800

    Moves FuzzySet and FuzzyRule definitions to separate files
    Moves the definitions of the `FuzzySet` and `FuzzyRule` classes from the `FDSHAEngine.h` header file to their own respective header files (`FuzzySet.h` and `FuzzyRule.h`).
    This improves code organization and modularity by separating concerns and reduces header file dependencies.

--- FDSHAEngine.h
+++ FDSHAEngine.h
@@ -2,65 +2,42 @@
 #define FDSHAENGINE_H
 
 #include "Enums.h"
+#include "FuzzySet.h"
+#include "FuzzyRule.h"
 #include <map>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <cmath>
 
 namespace FDSHA {
-
-    struct FuzzyRule {
-        MagnitudeTerm Mmax;
-        DistanceTerm R;
-        FaultTypeTerm F;
-        PGATerm PGA; // Consequent
-
-        // Constructor for easy initialization
-        FuzzyRule(MagnitudeTerm m, DistanceTerm r, FaultTypeTerm f, PGATerm p) : Mmax(m), R(r), F(f), PGA(p) {}
-
-        // Calculates rule's firing strength (alpha-cut) using MIN operator
-        double getFiringStrength(double mmaxDeg, double rDeg, double fDeg) const {
-            return std::min({mmaxDeg, rDeg, fDeg});
-        }
-    };
-
-    class FuzzySet {
-    public:
-        std::string Name;
-        // The core method to calculate degree of membership.
-        virtual double getMembershipDegree(double x) const = 0;
-        virtual ~FuzzySet() = default;
-    };
-
     class FDSHAEngine {
     private:
+        // Maps to hold fuzzy sets
         std::map<std::string, FuzzySet*> MmaxSets;
         std::map<std::string, FuzzySet*> RSets;
         std::map<std::string, FuzzySet*> FSets;
         std::map<std::string, FuzzySet*> PGASets;
         std::vector<FuzzyRule> Rules;
-        
+
+        // Initialization functions
         void initializeFuzzySets();
         void initializeRules();
 
-        // Helper to convert enums to strings
+        // Helper functions
         std::string enumToString(MagnitudeTerm t) const;
         std::string enumToString(DistanceTerm t) const;
         std::string enumToString(FaultTypeTerm t) const;
         std::string enumToString(PGATerm t) const;
 
-        // Defuzzification (to get the final crisp PGA)
+        // Defuzzification
         double defuzzifyCenterOfGravity(const std::map<PGATerm, double>& aggregatedConsequents) const;
 
     public:
         FDSHAEngine();
         ~FDSHAEngine();
 
-        // Main function to run the entire process and return the final PGA
         double findPGA(double mmaxInput, double rInput, double fInput);
     };
 
-} 
+} // namespace FDSHA
 
 #endif // FDSHAENGINE_H
